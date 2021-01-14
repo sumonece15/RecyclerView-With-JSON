e759599821b0af93c64b4d05ca9a09cb9c043b45
Sumon <sumonece15@gmail.com>
2021-01-14 12:22:11 +0600

    RecyclerView With JSON (1.0)

---
+++ app/src/main/java/com/example/recyclerviewwithjsonparsing/DetailsActivity.java
@@ -0,0 +1,36 @@
+package com.example.recyclerviewwithjsonparsing;
+
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import static com.example.recyclerviewwithjsonparsing.MainActivity.EXTRA_CREATOR;
+import static com.example.recyclerviewwithjsonparsing.MainActivity.EXTRA_LIKES;
+import static com.example.recyclerviewwithjsonparsing.MainActivity.EXTRA_URL;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+        String imageUrl = intent.getStringExtra(EXTRA_URL);
+        String creatorName = intent.getStringExtra(EXTRA_CREATOR);
+        int likeCount = intent.getIntExtra(EXTRA_LIKES, 0);
+
+        ImageView imageView = findViewById(R.id.image_view_detail);
+        TextView textViewCreator = findViewById(R.id.text_view_creator_detail);
+        TextView textViewLikes = findViewById(R.id.text_view_like_detail);
+
+        Picasso.get().load(imageUrl).fit().centerInside().into(imageView);
+        textViewCreator.setText(creatorName);
+        textViewLikes.setText("Likes: " + likeCount);
+    }
+}
